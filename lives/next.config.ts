3088774b59408a9c100f57d16e34a7402f944a63
Artjom Savin <aaartyomsss@gmail.com>
2025-11-30 15:12:20 +0200

    Path fixes

--- lives/next.config.ts
+++ lives/next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next"
 
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: "",
+  basePath: isProd ? "/lives" : "",
+  assetPrefix: isProd ? "/lives" : "",
 }
 
 export default nextConfig
